cf56c239126a3c74c7d2b7ba5691d516ce2a355c
Alexandr Gerasimov <san4y@yandex.ru>
2022-02-03 20:48:46 +0300

    ревью 4

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './components/modal.js';
 import './components/card.js';
 
 const trash = new URL('./images/Trash.svg', import.meta.url);
-const activeLike = new URL('./images/Union.svg', import.meta.url);
-const like = new URL('./images/Vector\ \(4\).svg', import.meta.url);
-const cross = new URL('./images/Close\ Icon.svg', import.meta.url);
-const edit = new URL('./images/Vector\ \(3\).svg', import.meta.url);
-const save = new URL('./images/Vector\ \(2\).svg', import.meta.url);
+const activeLike = new URL('./images/Active-like.svg', import.meta.url);
+const like = new URL('./images/Like.svg', import.meta.url);
+const cross = new URL('./images/Cross.svg', import.meta.url);
+const edit = new URL('./images/Edit.svg', import.meta.url);
+const save = new URL('./images/Save.svg', import.meta.url);
