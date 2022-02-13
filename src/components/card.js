698c6ea5f98b20997513045ce6d305565080eaad
Alexandr Gerasimov <san4y@yandex.ru>
2022-02-13 18:48:56 +0300

    ревью 3

--- src/components/card.js
+++ src/components/card.js
@@ -10,19 +10,35 @@ const name = document.getElementById('card-name');
 const link = document.getElementById('card-image'); 
 const namePopupImage = document.querySelector('.popup-image__picture')
 const namePopupTitle = document.querySelector('.popup-image__description')
-const likeCounterElement = document.querySelector('.element__like-counter');
 const cardButtonSelector = document.querySelector ('.popup__button_place');
 const avatarElement = document.querySelector('.profile__avatar');
 const profileName = document.querySelector('.profile-info__name');
 const profileStatus = document.querySelector('.profile-info__status');
+let userId = null
+//Добавление карточек
 
-    //Добавление карточек
+getAppInfo()
+  .then(([user, cards]) => {
+    profileName.textContent = user.name;
+    profileStatus.textContent = user.about;
+    avatarElement.src = user.avatar;
+    const userData = user._id
+    const initialCards = cards
+    initialCards.forEach((cardData) => {
+      cardList.prepend(createCard(cardData, userData))
+    })
+    userId = user._id
+  })
+  .catch(err => console.log(err));
+
+
 formCard.addEventListener('submit', function(evt) {
     evt.preventDefault();
     cardButtonSelector.textContent = 'Сохранение...';
     addNewCard(name.value, link.value)
-    .then((cardData, userId) => {
+    .then((cardData) => {
         addCard(cardData, cardList, userId)
+        console.log(userId)
         name.value = '';
         link.value = '';
         formCard.reset();
@@ -40,6 +56,7 @@ formCard.addEventListener('submit', function(evt) {
 
 export const addCard = (cardData, cardList, userId) => {
     const card = createCard(cardData, userId);
+    console.log(userId)
     cardList.prepend(card);
   };
 
@@ -66,8 +83,11 @@ export const createCard = (cardData, userId) => {
   likeElement.addEventListener('click', () => handleCardLike(likeElement, cardId, likeCounterElement));
 
   const deleteElement = cardElement.querySelector('.element__delete');
-  const isOwner = cardData.owner._id === userId;
-  deleteElement.classList.add(isOwner ? 'element__delete_active' : 'element__delete_hidden');
+  const isOwner = cardData.owner._id;
+    if (userId === isOwner) {
+        deleteElement.classList.add('element__delete_hidden')
+    }
+
   deleteElement.addEventListener('click', () => handleDeleteClick(cardElement, cardId));
 
   imageElement.addEventListener('click', function (evt) {
@@ -81,7 +101,6 @@ export const createCard = (cardData, userId) => {
   imageElement.src = cardData.link
   imageElement.alt = cardData.name
   titleElement.textContent = cardData.name
-
   return cardElement
 };
 
@@ -112,74 +131,3 @@ const handleCardLike = (likeElement, cardId, likeCounterElement) => {
       })
       .catch(err => console.log('Не удалось удалить карточку'));
   };
-  
-
-    /* formCard.addEventListener('submit', function(evt) {
-    evt.preventDefault();
-    cardButtonSelector.textContent = 'Сохранение...';
-    addNewCard(name.value, link.value)
-    .then((cardData) => {
-        addCard = (cardData, cardList, currentUserId)
-        name.value = '';
-        link.value = '';
-        formCard.reset();
-        cardButtonSelector.classList.add('popup__button_disabled')
-        cardButtonSelector.setAttribute('disabled', true)
-        closePopup(cardPopup)
-        location.reload()
-    })
-    .catch((err) => {
-        console.log(err);
-    })
-    .finally(() => {
-        cardButtonSelector.textContent = 'Создать';
-    })
-});
-
-export const addCard = (cardData, cardList, currentUserId) => {
-    const card = createCard(cardData, currentUserId);
-    cardList.prepend(card);
-  };
-
-closeImageButton.addEventListener('click', function () {
-    closePopup(imagePopup);
-});
-const cardTemplate = document.querySelector('#card-template').content;
-
-export const createCard = (cardData, currentUserId, cardUserId) => { 
-  const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
-  const imageElement = cardElement.querySelector('.element__image');
-  const titleElement = cardElement.querySelector('.element__text');
-  const likeElement = cardElement.querySelector('.element__like');
-  const likeCounterElement = cardElement.querySelector('.element__like-counter');
-  likeCounterElement.textContent = cardData.likes.length.toString();
-  const cardId = cardData._id
-  const isLiked = Boolean(cardData.likes.find(user => user._id === currentUserId));
-    if (isLiked) {
-        likeElement.classList.add('element__like_active');
-    } else {
-        likeElement.classList.remove('element__like_active');
-    }
-    
-  likeElement.addEventListener('click', () => handleCardLike(likeElement, cardId, likeCounterElement));
-
-  const deleteElement = cardElement.querySelector('.element__delete');
-  const isOwner = cardUserId === currentUserId;
-  deleteElement.classList.add(isOwner ? 'element__delete_active' : 'element__delete_hidden');
-  deleteElement.addEventListener('click', () => handleDeleteClick(cardElement, cardId));
-
-  imageElement.addEventListener('click', function (evt) {
-      evt.preventDefault();
-      namePopupImage.src = cardData.link
-      namePopupImage.alt = cardData.name
-      namePopupTitle.textContent = cardData.name
-      openPopup(imagePopup);
-  });
-
-  imageElement.src = cardData.link
-  imageElement.alt = cardData.name
-  titleElement.textContent = cardData.name
-
-  return cardElement
-};
-    */
